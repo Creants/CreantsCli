5984bdc5698433ab05e85e659a9c4ce1358d081e
bigO <coins@Nhas-MacBook-Pro.local>
2017-03-23 16:03:42 +0700

    add App

---
+++ src/app.js
@@ -0,0 +1,25 @@
+'use-strict'
+
+
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+
+class  CreantsCli extends Component {
+
+
+    render () {
+      return (
+        <View style={{flex: 1}}>
+          <Text>
+            HI Creants!
+          </Text>
+        </View>
+      );
+    }
+
+
+}
+
+
+module.exports = CreantsCli;
